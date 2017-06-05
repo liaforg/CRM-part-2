e63ee9adc4996bc6c6b43b0d22f4e48ef731aff3
Lia Forgione <liaforg@gmail.com>
2017-06-05 12:59:56 -0400

    git part 4 with new contact page

--- crm.rb
+++ crm.rb
@@ -16,28 +16,39 @@ end
 get '/contacts' do
   @contacts = Contact.count
   @contacts = Contact.all
+  puts params
   erb :contacts
 end
 
+get '/contacts/add_contact' do
+  erb :add_contact
+end
+
+get '/contacts/add_contact' do
+    @contact = Contact.create(params[:contact])
+    erb :add_contact
+end
+
+
 get '/contacts/:id' do
   # params[:id] contains the id from the URL
-  @contact = Contact.find(params[:id].to_i)
+  @contact = Contact.find_by(params[:id].to_i)
     if @contact
       erb :show_contact
     else
       raise Sinatra::NotFound
     end
-  erb :show_contact
 end
 
 
-get '/about' do
+get '/contacts/about' do
   erb :about
 end
 
-get '/add_contact' do
-  erb :add_contact
-end
+
+
+
+
 
 
 
