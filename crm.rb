cb21782182bc2246776392b64fc7e4848a569724
Lia Forgione <liaforg@gmail.com>
2017-06-05 15:26:34 -0400

    completed part 4

--- crm.rb
+++ crm.rb
@@ -19,10 +19,23 @@ get '/contacts' do
   erb :contacts
 end
 
+get '/contacts/:id' do
+  # params[:id] contains the id from the URL
+  @contact = Contact.find_by(params[:id].to_i)
+    if @contact
+      erb :show_contact
+    else
+      raise Sinatra::NotFound
+    end
+end
 
-get '/contacts/add_contact' do
-    @contact = Contact.create
+
+get '/add_contact' do
     erb :add_contact
+end
+
+get '/add_contact' do
+    @contacts = Contact.create(params[:contact])
 end
 
 post '/contacts' do
@@ -35,18 +48,50 @@ post '/contacts' do
   redirect to('/contacts')
 end
 
-get '/contacts/:id' do
-  # params[:id] contains the id from the URL
+get '/contacts/:id/edit' do
+  @contact = Contact.find(params[:id].to_i)
+  if @contact
+    erb :edit_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+put '/contacts/:id' do
+  @contact = Contact.find(params[:id].to_i)
+  if @contact
+    @contact.update(
+    first_name: params[:first_name],
+    last_name:  params[:last_name],
+    email:      params[:email],
+    note:       params[:note]
+    )
+
+    redirect to('/contacts')
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+# /contacts/83/delete
+get '/contacts/:id/delete' do
   @contact = Contact.find_by(params[:id].to_i)
-    if @contact
-      erb :show_contact
-    else
-      raise Sinatra::NotFound
-    end
+  erb :delete
+end
+
+delete '/contacts/:id' do
+  @contact = Contact.find(params[:id].to_i)
+  if @contact
+    @contact.delete
+    redirect to('/contacts')
+  else
+    raise Sinatra::NotFound
+  end
 end
 
 
-get '/contacts/about' do
+
+get '/about' do
   erb :about
 end
 
